3702c0e94765f0936b75b743d83492e22fb90ac9
Matyas Gombos <gombosmm@gmail.com>
2023-01-19 17:38:44 -0500

    Fix error raising function and syntax

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -10,15 +10,15 @@ describe Solver do
   end
 
   describe '#factorial' do
-    it 'should return an error message when N is smaller than 0' do
-      expect(@solver.factorial(-1)).to raise_exception
+    it 'should return an error message when n is smaller than 0' do
+      expect{ @solver.factorial(-1) }.to raise_exception(RuntimeError)
     end
 
-    it 'should return 1 when N is 0' do
+    it 'should return 1 when n is 0' do
       expect(@solver.factorial(0)).to eq(1)
     end
 
-    it 'should the multiplication of all integers from 1 to N' do
+    it 'should the multiplication of all integers from 1 to n' do
       expect(@solver.factorial(1)).to eq(1)
       expect(@solver.factorial(5)).to eq(120)
       expect(@solver.factorial(10)).to eq(3628800)
