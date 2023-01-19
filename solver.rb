79a62bb6eef5f27815b39a5dcb6ae9f38782fba4
Matyas Gombos <gombosmm@gmail.com>
2023-01-19 17:38:55 -0500

    Add correct factorial function

--- solver.rb
+++ solver.rb
@@ -1,2 +1,12 @@
 class Solver
+  def factorial(n)
+    case
+    when n < 0
+      raise 'n must be greater than or equal to 0'
+    when n == 0
+      1
+    else
+      (1..n).inject(:*)
+    end
+  end
 end
